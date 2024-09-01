04e2ba5aec025e653e656d1916a16f58369e1549
Ryan Ye <yejianye@gmail.com>
2024-09-01 17:07:14 +0800

    Automated commit on Sun Sep  1 17:07:14 CST 2024

--- emacs/my-pkgs/ry-tablex.el
+++ emacs/my-pkgs/ry-tablex.el
@@ -5,19 +5,26 @@
 ;; Create
 (defun ry/org-tablex-create-interactively ()
   (interactive)
-  (let* (column-names ( (read-string "Enter column names separated by comma: ")
-                          (s-split)))))
+  (let* ((column-names (thread-last (read-string "Enter column names separated by comma: ")
+                          (s-split ",")
+                          (--map (s-trim it))))
+         (table-id (ry/tablex-create column-names))
+         (table-block (format "#+begin: tablex :id %s\n#+end:" table-id)))
+    (insert table-block)))
 
 
-;; Render
 (defun org-dblock-write:tablex (params)
   (let* ((table-id (plist-get params :id))
          (output (gethash "display" (ry/tablex-render table-id)))
          (prop-output (propertize output 'line-spacing 0)))
     (insert prop-output)))
 
+;; Tablex Core Wrapper
 (defun ry/tablex-render (table-id)
   (ry/pyfunc "rypy.tablex" "table_render" table-id))
+
+(defun ry/tablex-create (column-names)
+  (ry/pyfunc "rypy.tablex" "table_create" column-names))
 
 (defun ry/tablex-register-font-face ()
   (font-lock-add-keywords
