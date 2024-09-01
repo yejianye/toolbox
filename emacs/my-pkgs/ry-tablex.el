dcfea7663ecde363e420b1bc080a03a874fb89c4
Ryan Ye <yejianye@gmail.com>
2024-09-02 00:11:26 +0800

    Automated commit on Mon Sep  2 00:11:26 CST 2024

--- emacs/my-pkgs/ry-tablex.el
+++ emacs/my-pkgs/ry-tablex.el
@@ -22,20 +22,17 @@
          (buffer (generate-new-buffer (format "*Table %s*" table-id)))
          (table-content (ry/tablex-get-table table-id t)))
     (pop-to-buffer buffer)
-    (insert "# Use `, c c` to commit the changes; use `, c k` to abort the changes.\n")
     (insert table-content)
     (yaml-mode)
+    (tablex-edit-mode)
+    (evil-normal-state)
     (setq-local ry/tablex-source-table-id table-id)
     (setq-local ry/tablex-source-buffer src-buffer)))
 
 (define-minor-mode tablex-edit-mode
   "A minor mode to edit tablex"
   :lighter "tablex"
-  :keymap (let ((map (make-sparse-keymap)))
-            (evil-define-key 'normal map
-              ",cc" 'ry/tablex-commit-changes
-              ",ck" 'ry/tablex-abort-changes)
-            map)
+  :keymap (make-sparse-keymap)
   (if tablex-edit-mode
       (message "tablex-edit-mode enabled")
     (message "tablex-edit-mode disabled")))
@@ -44,22 +41,24 @@
   (interactive)
   (let* ((content (buffer-substring-no-properties (point-min) (point-max)))
          (buffer (current-buffer))
-         (src-buffer (ry/tablex-source-buffer))
-         (table-id (ry/tablex-source-table-id)))
+         (src-buffer ry/tablex-source-buffer)
+         (table-id ry/tablex-source-table-id))
     (ry/tablex-save-raw table-id content)
     (delete-window)
     (kill-buffer buffer)
-    (display-buffer-reuse-window src-buffer)
+    (display-buffer src-buffer '(display-buffer-reuse-window))
     (goto-char (point-min))
     (re-search-forward (format ":id %s" table-id))
     (org-ctrl-c-ctrl-c)))
 
 (defun ry/tablex-abort-changes ()
   (interactive)
-  (let ((buffer (current-buffer)))
+  (let ((buffer (current-buffer))
+        (src-buffer ry/tablex-source-buffer))
     (when (yes-or-no-p "Are you sure to abort the changes?")
         (delete-window)
-        (kill-buffer buffer))))
+        (kill-buffer buffer)
+        (display-buffer src-buffer '(display-buffer-reuse-window)))))
 
 (defun org-dblock-write:tablex (params)
   (let* ((table-id (plist-get params :id))
@@ -103,6 +102,9 @@
 (defun ry/tablex-init ()
   (unless ry/tablex-init-completed
     (message "Initialize ry/tablex module")
+    (evil-define-key 'normal tablex-edit-mode-map
+      ",cc" 'ry/tablex-commit-changes
+      ",ck" 'ry/tablex-abort-changes)
     (ry/tablex-register-font-face)
     (setq ry/tablex-init-completed t)))
 
