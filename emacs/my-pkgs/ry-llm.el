ea52e6dcf30545e57b39edf366d0e76b6f40e2bc
Ryan Ye <yejianye@gmail.com>
2025-01-22 23:57:25 +0800

    Automated commit on Wed Jan 22 23:57:24 CST 2025

---
+++ emacs/my-pkgs/ry-llm.el
@@ -0,0 +1,9 @@
+;;; ry-llm.el --- LLM related functions -*- lexical-binding: t -*-
+
+;;; Commentary:
+;; Functions for interacting with LLM services
+
+;;; Code:
+
+(provide 'ry-llm)
+;;; ry-llm.el ends here
